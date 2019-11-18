d0fdb389136e0b8f4febfdfaa1d92f7e87ed690d
Nitin Chaurasia <gs.nitin@gmail.com>
2019-11-18 02:27:37 -0500

    API Gateway setup prepared

---
+++ src/test/java/com/nitin/microservices/learning/zuulGateServer/netflixzuulapigatewayserver/NetflixZuulApiGatewayServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nitin.microservices.learning.zuulGateServer.netflixzuulapigatewayserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetflixZuulApiGatewayServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
